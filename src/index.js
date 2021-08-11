5ee83472854a965a58e891683bd8743500565a14
Siarhei Saltanovich <siarhei_saltanovich@epam.com>
2021-08-11 14:38:19 +0300

    feat: add heroku app

--- src/index.js
+++ src/index.js
@@ -1,16 +1,24 @@
 const WebSocket = require('ws');
+const express = require('express');
 const Chat = require('./classes/Chat');
 const FormatterData = require('./classes/FormatterData');
 const User = require('./classes/User');
 
+const allowOrigin = 'https://chat-client-nodejs.netlify.app/';
+const PORT = process.env.PORT || 8080;
+const server = express()
+	.use((res) => {
+		res.setHeader('Access-Control-Allow-Origin', allowOrigin);
+		res.send('Connected');
+	})
+	.listen(PORT, () => console.log(`Listening on ${PORT}`));
+
 const chat = new Chat();
 const formatterData = new FormatterData(chat.chats);
 
-const server = new WebSocket.Server({
-	port: 8080,
-});
+const serverSocket = new WebSocket.Server({ server });
 
-server.on('connection', (socket) => {
+serverSocket.on('connection', (socket) => {
 	const currentUser = new User(socket, chat);
 	const mainChat = chat.getMainChat();
 	chat.pushUserInCurrChat(mainChat, currentUser);
